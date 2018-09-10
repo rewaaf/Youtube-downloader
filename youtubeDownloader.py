4e6d00d30823f618deb5be73e1727157509e7c79
Arwa <42501964+rewaaf@users.noreply.github.com>
2018-09-10 13:04:00 +0300

    Update youtubeDownloader.py

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,5 +1,7 @@
 # single video
+
 from pytube import YouTube
+
 link = input('Hello to YouTube Downloader\nPlease enter the link of the video: ')
 video = YouTube(link)
 vname = video.title
@@ -7,16 +9,17 @@ print('The video title is: '+vname)
 quality = video.streams.filter(mime_type="video/mp4").all()
 print('Video quality: ')
 for qua in quality:
-    #print(qua)
     print(qua.resolution)
 selection = input('Enter video quality such as (720p): ')
 selectedvideo = video.streams.filter(res=selection).first()
-selectedvideo.download('/put your path')
+selectedvideo.download('/put your path here')
 print('Thank you for downloading!')
 
 # playlist
+
 from pytube import Playlist
+
 link = input('Please enter the link of the playlist: ')
 playlist = Playlist(link)
-playlist.download_all('/put your path')
+playlist.download_all('/put your path here')
 print('Thank you for downloading!')
