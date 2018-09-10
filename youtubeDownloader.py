10e434e3f2a9fe16730264d09a8b7c9f6f4859df
Arwa <42501964+rewaaf@users.noreply.github.com>
2018-09-10 13:01:35 +0300

    Create youtubeDownloader.py

---
+++ youtubeDownloader.py
@@ -0,0 +1,22 @@
+# single video
+from pytube import YouTube
+link = input('Hello to YouTube Downloader\nPlease enter the link of the video: ')
+video = YouTube(link)
+vname = video.title
+print('The video title is: '+vname)
+quality = video.streams.filter(mime_type="video/mp4").all()
+print('Video quality: ')
+for qua in quality:
+    #print(qua)
+    print(qua.resolution)
+selection = input('Enter video quality such as (720p): ')
+selectedvideo = video.streams.filter(res=selection).first()
+selectedvideo.download('/put your path')
+print('Thank you for downloading!')
+
+# playlist
+from pytube import Playlist
+link = input('Please enter the link of the playlist: ')
+playlist = Playlist(link)
+playlist.download_all('/put your path')
+print('Thank you for downloading!')
